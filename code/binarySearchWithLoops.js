ce0737252256b2c03a2e9f556960a18278a67f14
awwmicky <michael_fred_alvarez@yahoo.com>
2020-06-21 20:38:58 -0700

    added code snippets

---
+++ code/binarySearchWithLoops.js
@@ -0,0 +1,91 @@
+const defaultCompare = (a,b) => {
+    if (a > b) return 1;
+    else if (a < b) return -1; 
+    else return 0;
+};
+  
+/* rules: O(n) | fixed list | loop */
+
+// OPTION - 1
+
+function binarySearchForLoop (
+    arr , elm , compare = defaultCompare
+  ) {
+    let left = 0; 
+    let right = arr.length - 1;
+  
+    for ( left; left <= right; left ) {
+        let middle = Math.floor( (right + left) / 2 );
+        let arrm = compare( elm,arr[middle] );
+        
+        if (arrm === -1) {
+            console.log('move left ←')
+            right = middle - 1;
+        } else if (arrm === 1) {
+            console.log('move right →')
+            left = middle + 1;
+        } else {
+            console.log('result:', arr[middle])
+            return middle;
+        }
+    }
+  
+    console.log( 'nope', arr[elm] )
+    return -1; 
+};
+
+console.log(
+    // binarySearchForLoop( [],2 ) // -1
+    // binarySearchForLoop( [1],1 ) // 0
+    // binarySearchForLoop( [1],2 ) // -1
+    // binarySearchForLoop( [1,2,3],2 ) // 1
+    // binarySearchForLoop( [1,2,3],3 ) // 2 
+    // binarySearchForLoop( [1,2,3],4 ) // -1
+    // binarySearchForLoop( [1,2,3,7],3 ) // 2
+    // binarySearchForLoop( ['a','d','g','h'],'g' ) // 2
+    // binarySearchForLoop( [1,2,3,5,6,7,8],6 ) //
+    // binarySearchForLoop( ['a','d','l','p','w','z'],'d' ) //
+)
+
+/* ----- */
+
+// OPTION - 2
+
+function binarySearchWhileLoop (
+    arr, elm, compare = defaultCompare
+) {
+    let left = 0, right = arr.length - 1;
+    
+    while (left <= right) {
+        let middle = Math.floor( (right + left) / 2 );
+
+        // -1 | 0 | 1
+        switch ( compare( elm,arr[middle] ) ) {
+            case -1:
+                right = middle - 1;
+                break;
+            case 1:
+                left = middle + 1;
+                break;
+            default:
+                console.log('result:', arr[middle])
+                return middle;
+        }
+    }
+
+    console.log( 'nope', arr[elm] )
+    return -1; 
+};
+
+console.log(
+    // binarySearchWhileLoop( [],2 ) // -1
+    // binarySearchWhileLoop( [1],1 ) // 0
+    // binarySearchWhileLoop( [1],2 ) // -1
+    // binarySearchWhileLoop( [1,2,3],2 ) // 1
+    // binarySearchWhileLoop( [1,2,3],3 ) // 2 
+    // binarySearchWhileLoop( [1,2,3],4 ) // -1
+    // binarySearchWhileLoop( [1,2,3,7],3 ) // 2
+    binarySearchWhileLoop( ['a','d','g','h'],'g' ) // 2
+    // binarySearchWhileLoop( [1,2,3,5,6,7,8],6 ) //
+    // binarySearchWhileLoop( ['a','d','l','p','w','z'],'d' ) //
+)
---
+++ code/binarySearchWithRecursion.js
@@ -0,0 +1,84 @@
+const defaultCompare = (a,b) => {
+    return (a > b) ? 1 : (a < b) ? -1 : 0;
+};
+
+/* rules: O(n) | time | no loops | recursion */
+
+// OPTION - 1
+
+function binarySearchRecursion ( 
+    arr, elm, compare = defaultCompare, 
+    left = 0, right = arr.length - 1 
+) {
+    console.log(left > right)
+    if ( left > right ) return -1;
+
+    const middle = Math.floor( (right + left) / 2 );
+    const comparison = compare( elm, arr[middle] );
+    console.log('L or R:', comparison)
+
+    if (comparison === -1 ) {
+        console.log('move left ←', middle)
+        return binarySearchRecursion( arr,elm,compare, left,(middle-1) )
+    } else if (comparison === 1 ) {
+        console.log('move right →', middle)
+        return binarySearchRecursion( arr,elm,compare, (middle+1),right )
+    } else {
+        return middle;
+    }
+}
+
+console.log(
+    // binarySearchRecursion( [],2 ) // -1
+    // binarySearchRecursion( [1],1 ) // 0
+    // binarySearchRecursion( [1],2 ) // -1
+    // binarySearchRecursion( [1,2,3],2 ) // 1
+    // binarySearchRecursion( [1,2,3],3 ) // 2 
+    // binarySearchRecursion( [1,2,3],4 ) // -1
+    // binarySearchRecursion( [1,2,3,7],3 ) // 2
+    // binarySearchRecursion( ['a','d','g','h'],'g' ) // 2  
+    // binarySearchRecursion( [1,2,3,5,6,7,8],6 ) //
+    // binarySearchRecursion( ['a','d','l','p','w','z'],'d' ) //
+)
+
+/* ------ */
+
+// OPTION - 2
+
+function binarySearchTailRecursion ( 
+    arr, elm, compare = defaultCompare, 
+    left = 0, right = arr.length - 1 
+) {
+    console.log(left > right)
+    if ( left > right ) return -1;
+  
+    const middle = Math.floor( (right + left) / 2 );
+    const comparison = compare( elm, arr[middle] );
+    console.log('L or R:', comparison)
+    
+    console.log( comparison === 0 ? middle : '' )
+    if ( comparison === 0 ) return middle;
+    
+    const newBounds = (
+        (comparison === -1) 
+        ? [ left,(middle - 1) ] 
+        : [ (middle + 1),right ]
+    );
+  
+    return binarySearchTailRecursion( 
+        arr,elm,compare, ...newBounds
+    );
+}
+
+console.log(
+    // binarySearchTailRecursion( [],2 ) // -1
+    // binarySearchTailRecursion( [1],1 ) // 0
+    // binarySearchTailRecursion( [1],2 ) // -1
+    // binarySearchTailRecursion( [1,2,3],2 ) // 1
+    // binarySearchTailRecursion( [1,2,3],3 ) // 2 
+    // binarySearchTailRecursion( [1,2,3],4 ) // -1
+    // binarySearchTailRecursion( [1,2,3,7],3 ) // 2
+    binarySearchTailRecursion( ['a','d','g','h'],'g' ) // 2
+    // binarySearchTailRecursion( [1,2,3,5,6,7,8],6 ) //
+    // binarySearchTailRecursion( ['a','d','l','p','w','z'],'d' ) //
+)
